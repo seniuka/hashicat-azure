979aec0a8e0b4c7b4fc6db00a0fb9a7996d463d6
Adam <aseniuk@gmail.com>
2023-03-01 12:57:45 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
- Welcome to the Jungle! We ahve ${PREFIX} and games!
+ Welcome to the Jungle! We ahve ${PREFIX} and games?!!?
   <!-- END -->
   
   </div>
